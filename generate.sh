d824122f0853f5cf9a892abd61bd9b16a6818524
noodlefighter <noodlefighter@gmail.com>
2021-03-05 15:56:39 +0800

    修复图片无法显示的问题

--- generate.sh
+++ generate.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#
-# 文件名和目录名不允许带空格! 不想处理这种特殊情况了, 垃圾shell脚本
-#
-
 set -e
 
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
@@ -18,7 +14,7 @@ echo "=============================="
 echo " generate file"
 echo "=============================="
 cd $SOURCE_FOLDER
-find -name "*.md" -print0|xargs -0 -i python $SHELL_FOLDER/scripts/do_copy.py {} $BUILD_FOLDER
+find -name "*.md" -print0|xargs -0 -i python $SHELL_FOLDER/scripts/do_copy.py "{}" "$BUILD_FOLDER"
 
 echo "=============================="
 echo " copy to _post"
@@ -32,3 +28,4 @@ cd $SHELL_FOLDER/hexo
 yarn install
 yarn run hexo clean
 yarn run hexo g
+yarn run hexo g # quick fix for image-asset bug "undefined"
